a1eb42d2308a6c4d1f0038f10bea2021a5a278f2
ouma evans <evansotis2015@gmail.com>
2019-11-26 02:51:13 +0300

    Added the lat and lon values to the DOM

--- index.js
+++ index.js
@@ -4,22 +4,6 @@ let placeInput = document.querySelector('#thisPlace');
 
 
 
-const urlString = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyBEXI57STEvdC3BZhM7JEDdh5gFmClbGJM";
-
-
-	function initMap(){
-
-		var mapy = new google.maps.Map(document.getElementById('maparea'), 
-			{
-				zoom: 4, 
-				center: {lat: 42.3601, lng: -71.0589}
-			});
-
-		var marker = new google.maps.Marker({
-			position: {lat: 42.3601, lng: -71.0589}, 
-			map: mapy
-		});
-	}
 
 
 searchForm.addEventListener('submit', ($e) => {
@@ -33,28 +17,38 @@ searchForm.addEventListener('submit', ($e) => {
 		var tempOutput = data['main']['temp'];
 		var humidOutput = data['main']['humidity'];
 		var weatherDesc = data['weather'][0]['description'];
+		var lonOut = data['coord']['lon'];
+		var latOut = data['coord']['lat'];
+
+		document.getElementById('farenH').style.display = "inline";
 
 		document.getElementById('count-name').innerHTML = nameOutput;
 		document.getElementById('speed').innerHTML = 'Wind speed: '+ windOutput;
 		document.getElementById('temp').innerHTML = 'Temperatures: ' + tempOutput + ' &deg;K';
 		document.getElementById('humid').innerHTML = 'Humidity: ' + humidOutput;
 		document.getElementById('desc').innerHTML = 'Weather: ' + weatherDesc;
+		document.getElementById('long').innerHTML = lonOut;
+		document.getElementById('lati').innerHTML = latOut;
 
 		let farenHeit = (tempOutput-273.15)*(9/5) + 32;
 
 		let celCius = (farenHeit-32)*(5/9);
 
+		// Converting to FarenHeit
 		document.getElementById('farenH').addEventListener('click', () => {
 			document.getElementById('temp').innerHTML = 'Temperatures: ' + farenHeit.toFixed(2) + ' &deg;F';
 			document.getElementById('farenH').style.display = 'none';
 			document.getElementById('celC').style.display = 'inline';
 		});
 
+		// Converting to Degrees Celcius
 		document.getElementById('celC').addEventListener('click', () => {
 			document.getElementById('temp').innerHTML = 'Temperatures: ' + celCius.toFixed(2) + ' &deg;C';
 			document.getElementById('celC').style.display = 'none';
 			document.getElementById('farenH').style.display = 'inline';
 		});
+
+		
 
 	})
 	.catch( error => alert("Wrong City name Try again!"));
